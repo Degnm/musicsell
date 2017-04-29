7164ee2c09459fb40163046c8422aa7ab53bcf92
Nataha <degnm@yandex.ru>
2017-04-29 19:31:58 +0300

    Добавлено количество авторов

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///%s' % \
+                          os.path.join(BASE_DIR, 'Chinook_Sqlite.sqlite')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 DEBUG = True
 
 app = Flask(__name__, static_folder='node_modules', static_url_path='/node_modules')
 app.config.from_object(__name__)
+db = SQLAlchemy(app)
 
 import views
--- views.py
+++ views.py
@@ -1,9 +1,16 @@
 from flask import render_template
 
-from app import app
+from app import app, db
 from data import menu
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', main_manu=menu)
+    authors_count = db.engine.execute('''
+    SELECT COUNT(*)
+    FROM Artist
+    ''')
+    return render_template('index.html', **{
+        'author_count': authors_count.scalar(),
+        'main_menu': menu,
+    })
