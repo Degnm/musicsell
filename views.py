49cf77caada23d463addcb7b7e4af0e5a30edeb9
Nataha <degnm@yandex.ru>
2017-05-01 23:30:51 +0300

    Add artists

--- views.py
+++ views.py
@@ -10,7 +10,26 @@ def index():
     SELECT COUNT(*)
     FROM Artist
     ''')
+    albums_count = db.engine.execute('''
+    SELECT COUNT(*)
+    FROM Album
+    ''')
+    genres_count = db.engine.execute('''
+       SELECT COUNT(*)
+       FROM Genre
+       ''')
     return render_template('index.html', **{
         'author_count': authors_count.scalar(),
         'main_menu': menu,
+        'album_count': albums_count.scalar(),
+        'genre_count': genres_count.scalar(),
     })
+
+
+@app.route('/artists')
+def all_artists():
+    artists = db.engine.execute('''
+    SELECT *
+    FROM Artist
+    ''')
+    return render_template('artists.html', artists=artists.fetchall())
